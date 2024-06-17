a07e0ce242cda6dadf3d6e32198c8600133f5b9a
Nguyen Dang Quy <dangquyitt@gmail.com>
2024-06-17 23:07:08 +0700

    Caculate the hash of a block

--- blockchain.go
+++ blockchain.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -13,12 +15,12 @@ func init() {
 
 type Block struct {
 	nonce        int
-	previousHash string
+	previousHash [32]byte
 	timestamp    int64
 	transactions []string
 }
 
-func NewBlock(nonce int, previousHash string) *Block {
+func NewBlock(nonce int, previousHash [32]byte) *Block {
 	return &Block{
 		nonce:        nonce,
 		previousHash: previousHash,
@@ -28,9 +30,28 @@ func NewBlock(nonce int, previousHash string) *Block {
 
 func (b *Block) Print() {
 	fmt.Printf("nonce: %d\n", b.nonce)
-	fmt.Printf("previousHash: %s\n", b.previousHash)
+	fmt.Printf("previousHash: %x\n", b.previousHash)
 	fmt.Printf("timestamp: %d\n", b.timestamp)
 	fmt.Printf("transactions: %s\n", b.transactions)
+}
+
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	return sha256.Sum256([]byte(m))
+}
+
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Nonce        int      `json:"nonce"`
+		PreviousHash [32]byte `json:"previous_hash"`
+		Timestamp    int64    `json:"timestamp"`
+		Transactions []string `json:"transactions"`
+	}{
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Timestamp:    b.timestamp,
+		Transactions: b.transactions,
+	})
 }
 
 type BlockChain struct {
@@ -39,15 +60,20 @@ type BlockChain struct {
 }
 
 func NewBlockChain() *BlockChain {
+	b := &Block{}
 	bc := new(BlockChain)
-	bc.CreateBlock(0, "Init hash")
+	bc.CreateBlock(0, b.Hash())
 	return bc
 }
 
-func (bc *BlockChain) CreateBlock(nonce int, previousHash string) *Block {
+func (bc *BlockChain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b)
 	return b
+}
+
+func (bc *BlockChain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
 }
 
 func (bc *BlockChain) Print() {
@@ -61,8 +87,12 @@ func (bc *BlockChain) Print() {
 func main() {
 	blockChain := NewBlockChain()
 	blockChain.Print()
-	blockChain.CreateBlock(5, "hash 1")
+
+	previousHash := blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(5, previousHash)
 	blockChain.Print()
-	blockChain.CreateBlock(2, "hash 2")
+
+	previousHash = blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(2, previousHash)
 	blockChain.Print()
 }
